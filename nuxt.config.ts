5641116459ee3a82314701a83671f7bf1e5bd6a0
ekud12 <elixir03@gmail.com>
2025-05-30 17:44:14 +0300

    ci

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css', '~/assets/css/fonts.css'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss'
+  ]
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+export default {
+    content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './app.vue',
+      './nuxt.config.{js,ts}'
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          PublicaPlay: ['PublicaPlay', 'sans-serif']
+        }
+      }    },
+    plugins: []
+  }
+  
