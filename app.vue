a9b176b1fbc02e25f34a4d74f286fba619639eaa
ekud12 <elixir03@gmail.com>
2025-05-30 22:36:11 +0300

    ci

--- app.vue
+++ app.vue
@@ -1,9 +1,8 @@
 <template>
   <div
-    class="flex min-h-screen flex-col items-center justify-center bg-black text-white"
+  class="flex min-h-[100dvh] flex-col items-center justify-center bg-black text-white"
   >
     <LogoIcon />
-
     <h1 class="ext-5xl text-4xl lg:text-7xl mt-6">bitmill.dev</h1>
     <p class="mt-4 text-base sm:text-lg text-gray-400">contact@bitmill.dev</p>
   </div>
@@ -12,18 +11,18 @@
 <script setup lang="ts">
 import LogoIcon from "~/components/LogoIcon.vue";
 
-// eslint-disable-next-line no-undef
+ 
 useHead({
   htmlAttrs: {
     lang: "en",
   },
-  title: "@ekud workspace - Modern Development Environment",
+  title: "bitmill.dev - Development studio",
   meta: [
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     {
       name: "description",
       content:
-        "@ekud workspace - Building the future of development environments",
+        "bitmill.dev - Development studio",
     },
     { name: "theme-color", content: "#00DC82" },
     { name: "color-scheme", content: "dark light" },
