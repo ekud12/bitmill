398c83ebd4a7ba0ccb9ea4172141ba3e5a8d4ba7
ekud12 <elixir03@gmail.com>
2025-05-30 18:31:04 +0300

    ci

--- app.vue
+++ app.vue
@@ -2,9 +2,8 @@
   <div
     class="flex min-h-screen flex-col items-center justify-center bg-black text-white"
   >
-    <div class="max-w-[600px] w-full aspect-square">
-      <LogoIcon class="w-full h-full" />
-    </div>
+  <LogoIcon />
+
     <h1 class="ext-5xl sm:text-6xl mt-6">bitmill.dev</h1>
     <p class="mt-4 text-base sm:text-lg text-gray-400">contact@bitmill.dev</p>
   </div>
