fa0b8ee35478ef1182044e58ca11352d56e42d5a
ekud12 <elixir03@gmail.com>
2025-06-09 13:31:54 +0300

    added icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,17 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
